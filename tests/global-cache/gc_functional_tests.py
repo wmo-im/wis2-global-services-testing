fdf115e8ad0d2c7d99c55d375de0bf43cbf2501d
jmaxmarno <jmaxmarno@gmail.com>
2024-08-21 21:17:05 -0600

    add fourth test and shared utils stub

--- tests/global-cache/gc_functional_tests.py
+++ tests/global-cache/gc_functional_tests.py
@@ -16,16 +16,17 @@ from pywis_pubsub.verification import verify_data
 from pywis_pubsub.publish import create_message
 from pywis_pubsub.schema import sync_schema as pw_sync
 from pywis_pubsub.mqtt import MQTTPubSubClient
+from dotenv import load_dotenv
+load_dotenv()
 # Add the parent directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from shared_utils import mqtt_helpers
 
 
 # Connection strings for the development global broker and message generator
-mqtt_broker_out = "mqtts://everyone:everyone@dev-cache.wis2.synopticdata.com:8883"
-mqtt_broker_in = "mqtts://everyone:everyone@emqx.wis2.synopticdata.com:8883"
-
+# Access the environment variables
+mqtt_broker_out = os.getenv('MQTT_BROKER_OUT')
+mqtt_broker_in = os.getenv('MQTT_BROKER_IN')
 # Topics
 sub_topics = [
     "cache/a/wis2/#",
@@ -93,51 +94,6 @@ def setup_mqtt_client(connection_info: str):
     return client
 
 
-def test_1_gc_mqtt_broker_connectivity():
-    print("\n1. MQTT Broker Connectivity")
-    assert mqtt_helpers.check_broker_connectivity(mqtt_broker_out) is True
-
-
-@pytest.mark.parametrize("topic", [
-    "cache/a/wis2/+/data/core/#",
-    "cache/a/wis2/+/metadata/#"
-])
-def test_2_gc_mqtt_broker_subscription(topic):
-    print("\n2. GC MQTT Broker Subscription")
-    print(f"Subscribing to topic: {topic}")
-
-    client = setup_mqtt_client(mqtt_broker_out)
-    client.subscribed_flag = False
-    result, mid = client.subscribe(topic)
-    time.sleep(.1)  # Wait for subscription
-    assert result is mqtt.MQTT_ERR_SUCCESS
-    assert client.subscribed_flag is True
-    client.loop_stop()
-    client.disconnect()
-    del client
-
-@pytest.mark.parametrize("topic", [
-    "cache/a/wis2/+/data/core/#",
-    "cache/a/wis2/+/metadata/#"
-])
-def test_2test_gc_mqtt_broker_subscription(topic):
-    print("\n2. GC MQTT Broker Subscription")
-    print(f"Subscribing to topic: {topic}")
-    # use pywispubsub client but specify the on_connect, on_subscribe callbacks
-    client = MQTTPubSubClient(mqtt_broker_out)
-    client.subscribed_flag = False
-    client.conn.on_subscribe = flag_on_subscribe
-    client.conn.on_connect = flag_on_connect
-    client.conn.loop_start()
-    result, mid = client.conn.subscribe(topic)
-    time.sleep(1)  # Wait for subscription
-    assert result is mqtt.MQTT_ERR_SUCCESS
-    assert client.conn.subscribed_flag is True
-    client.conn.loop_stop()
-    client.conn.disconnect()
-    del client
-
-
 def generate_wnm(topic: str = None, msg_id: str = None, msg_dt: datetime = None, url: str = None,
                  content_type: str = None, msg_props: dict = None):
     topic = topic or get_random_topic()
@@ -161,8 +117,33 @@ def generate_wnm(topic: str = None, msg_id: str = None, msg_dt: datetime = None,
     return topic, message
 
 
+def test_1_mqtt_broker_connectivity():
+    print("\n1. MQTT Broker Connectivity")
+    assert mqtt_helpers.check_broker_connectivity(mqtt_broker_out) is True
+
+@pytest.mark.parametrize("topic", [
+    "cache/a/wis2/+/data/core/#",
+    "cache/a/wis2/+/metadata/#"
+])
+def test_2_mqtt_broker_subscription(topic):
+    print("\n2. GC MQTT Broker Subscription")
+    print(f"Subscribing to topic: {topic}")
+    # use pywispubsub client but specify the on_connect, on_subscribe callbacks
+    client = MQTTPubSubClient(mqtt_broker_out)
+    client.subscribed_flag = False
+    client.conn.on_subscribe = flag_on_subscribe
+    client.conn.on_connect = flag_on_connect
+    client.conn.loop_start()
+    result, mid = client.conn.subscribe(topic)
+    time.sleep(1)  # Wait for subscription
+    assert result is mqtt.MQTT_ERR_SUCCESS
+    assert client.conn.subscribed_flag is True
+    client.conn.loop_stop()
+    client.conn.disconnect()
+    del client
+
 @pytest.mark.parametrize("pub_topic", pub_topics)
-def test_mqtt_broker_message_flow(pub_topic):
+def test_3_mqtt_broker_message_flow(pub_topic):
     print("\n3. WIS2 Notification Message (WNM) Processing")
     # generate some random id's for the messages
     sub_client = setup_mqtt_client(mqtt_broker_out)
@@ -180,6 +161,7 @@ def test_mqtt_broker_message_flow(pub_topic):
     sent_messages = []
     # add random salt to pubtopic, 6 hex characters
     pub_topic = "/".join([pub_topic + uuid.uuid4().hex[:6]])
+    # todo - trigger WNM's from the message generator, rather than generate here
     wnm = generate_wnm(pub_topic)
     pub_client = MQTTPubSubClient(mqtt_broker_in)
     # publish the message
@@ -205,10 +187,59 @@ def test_mqtt_broker_message_flow(pub_topic):
         # assert the msg id's are different
         assert cache_msg['id'] != origin_msg['id']
         # assert the links['rel']='canonical' are different
-        assert cache_msg['links'][0]['href'] != origin_msg['links'][0]['href']
+        # get the index of the link with rel=canonical
+        can_i_cache = [i for i, l in enumerate(cache_msg['links']) if l['rel'] == 'canonical'][0]
+        can_i_origin = [i for i, l in enumerate(origin_msg['links']) if l['rel'] == 'canonical'][0]
+        assert cache_msg['links'][can_i_cache]['href'] != origin_msg['links'][can_i_origin]['href']
          # use pywispubsub to validate the cache messages
         is_valid, errors = validate_message(cache_msg)
         assert is_valid is True
         # verification
         verified = verify_data(cache_msg)
         assert verified is True
+
+def test_4_cache_false_directive():
+    print("\n4. Cache False Directive")
+
+    # Setup MQTT client
+    sub_client = setup_mqtt_client(mqtt_broker_out)
+    sub_client.subscribed_flag = False
+    sub_client.subscribe("cache/a/wis2/#", qos=1)
+    sub_client.subscribe("origin/a/wis2/#", qos=1)
+    time.sleep(1)  # Wait for subscription
+
+    # Prepare WIS2 Notification Messages
+    sent_messages = []
+    for _ in range(5):  # Send 5 messages for the test
+        pub_topic = get_random_topic()
+        wnm = generate_wnm(pub_topic, msg_props={'cache': False})
+        pub_client = MQTTPubSubClient(mqtt_broker_in)
+        pub_client.pub(topic=pub_topic, message=json.dumps(wnm[1]))
+        sent_messages.append(wnm[1])
+        print(f"Published message with data_id: {wnm[1]['properties']['data_id']} and datetime: {wnm[1]['properties']['datetime']}")
+
+    time.sleep(10)  # Wait for messages
+
+    # Evaluate
+    received_msgs = [json.loads(msg) for msg in sub_client._userdata['received_messages']]
+    sub_client.loop_stop()
+    sub_client.disconnect()
+
+    for origin_msg in sent_messages:
+        cache_msg = [m for m in received_msgs if
+                     m['properties']['data_id'] == origin_msg['properties']['data_id'] and m['properties'][
+                         'datetime'] == origin_msg['properties']['datetime']]
+        assert len(cache_msg) == 1
+        cache_msg = cache_msg[0]
+        assert cache_msg['id'] != origin_msg['id']
+        assert cache_msg['links'][0]['href'] == origin_msg['links'][0]['href']
+        is_valid, errors = validate_message(cache_msg)
+        assert is_valid is True
+        verified = verify_data(cache_msg)
+        assert verified is True
+
+    # Check GC metrics (mocked for this test)
+    # assert gc_metrics['wmo_wis2_gc_no_cache_total'] == len(sent_messages)
+    # assert gc_metrics['wmo_wis2_gc_download_total'] == initial_download_total
+    # assert gc_metrics['wmo_wis2_gc_dataserver_status_flag'] == initial_status_flag
+    # assert gc_metrics['wmo_wis2_gc_dataserver_last_download_timestamp_seconds'] == initial_timestamp
