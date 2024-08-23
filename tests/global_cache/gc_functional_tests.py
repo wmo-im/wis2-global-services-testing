5e6b3c7950c3bb3ed45216bcef6ca6f97d68e894
jmaxmarno <jmaxmarno@gmail.com>
2024-08-23 13:54:22 -0600

    move wnm generation function to shared utils

--- tests/global_cache/gc_functional_tests.py
+++ tests/global_cache/gc_functional_tests.py
@@ -44,22 +44,6 @@ pub_topics = [
 # Initialize MQTT client
 rand_id = "wis2_testing" + str(uuid.uuid4())[:10]
 
-
-# Helper functions
-
-# get random topic
-def get_random_topic():
-    # select a random topic from below, and fill in the wildcards for a complete topic
-    topics = ["origin/a/wis2/+/data/#", "cache/a/wis2/+/data/#", "origin/a/wis2/+/metadata/#",
-              "cache/a/wis2/+/metadata/#"]
-    # get random
-    topic = topics[random.randint(0, len(topics) - 1)]
-    # fill in the wildcards
-    topic = topic.replace("+", str(uuid.uuid4()))
-    topic = topic.replace("#", str(uuid.uuid4()))
-    return topic
-
-
 def flag_on_connect(client, userdata, flags, rc, properties=None):
     # print("Connected with result code " + str(rc))
     if rc == 0:
@@ -97,29 +81,6 @@ def setup_mqtt_client(connection_info: str):
     if not client.is_connected():
         raise Exception("Failed to connect to MQTT broker")
     return client
-
-
-def generate_wnm(topic: str = None, msg_id: str = None, msg_dt: datetime = None, url: str = None,
-                 content_type: str = None, msg_props: dict = None):
-    topic = topic or get_random_topic()
-    msg_dt = msg_dt.strftime('%Y-%m-%dT%H:%M:%S') + 'Z' if msg_dt is not None else datetime.utcnow().strftime(
-        '%Y-%m-%dT%H:%M:%S') + 'Z'
-    # msg_dt = msg_dt if msg_dt is not None else datetime.utcnow()
-    message_args = {
-        'topic': topic,
-        'content_type': content_type,
-        'url': "https://wis2-global-cache.s3.amazonaws.com/br-inmet/data/core/weather/surface-based-observations/synop/WIGOS_0-76-0-3106200000000597_20240822T140000.bufr4",
-        'identifier': msg_id or str(uuid.uuid4()),
-        'pubtime': msg_dt,  #
-        # 'geometry': [33.8, -11.8, 123],
-        # 'metadata_id': 'mock/metadata/identifier',
-        # 'wigos_station_identifier': '0-20000-12345',
-        # 'operation': 'create'
-    }
-    if msg_props:
-        message_args.update(msg_props)
-    message = create_message(**message_args)
-    return topic, message
 
 
 def test_1_mqtt_broker_connectivity():
--- tests/shared_utils/mqtt_helpers.py
+++ tests/shared_utils/mqtt_helpers.py
@@ -1,7 +1,17 @@
+import uuid
+from datetime import datetime
+from random import random
+
 from pywis_pubsub.mqtt import MQTTPubSubClient
+from pywis_pubsub.publish import create_message
 
 
-def check_broker_connectivity(connection_string:str):
+def check_broker_connectivity(connection_string: str):
+    """
+    Check if the broker is reachable
+    :param connection_string:
+    :return:
+    """
     # return True if connection is successful, False otherwise
     is_connected = False
     pwclient = MQTTPubSubClient(connection_string)
@@ -12,3 +22,33 @@ def check_broker_connectivity(connection_string:str):
     pwclient.conn.disconnect()
     del pwclient
     return is_connected
+
+
+def generate_wnm(topic: str = None, msg_id: str = None, msg_dt: datetime = None, url: str = None,
+                 content_type: str = None, msg_props: dict = None):
+    if topic is None:
+        topics = ["origin/a/wis2/+/data/#", "cache/a/wis2/+/data/#", "origin/a/wis2/+/metadata/#",
+                  "cache/a/wis2/+/metadata/#"]
+        # get random
+        topic = topics[random.randint(0, len(topics) - 1)]
+        # fill in the wildcards
+        topic = topic.replace("+", str(uuid.uuid4()))
+        topic = topic.replace("#", str(uuid.uuid4()))
+    msg_dt = msg_dt.strftime('%Y-%m-%dT%H:%M:%S') + 'Z' if msg_dt is not None else datetime.utcnow().strftime(
+        '%Y-%m-%dT%H:%M:%S') + 'Z'
+    # msg_dt = msg_dt if msg_dt is not None else datetime.utcnow()
+    message_args = {
+        'topic': topic,
+        'content_type': content_type,
+        'url': "https://wis2-global-cache.s3.amazonaws.com/br-inmet/data/core/weather/surface-based-observations/synop/WIGOS_0-76-0-3106200000000597_20240822T140000.bufr4",
+        'identifier': msg_id or str(uuid.uuid4()),
+        'pubtime': msg_dt,  #
+        # 'geometry': [33.8, -11.8, 123],
+        # 'metadata_id': 'mock/metadata/identifier',
+        # 'wigos_station_identifier': '0-20000-12345',
+        # 'operation': 'create'
+    }
+    if msg_props:
+        message_args.update(msg_props)
+    message = create_message(**message_args)
+    return topic, message
