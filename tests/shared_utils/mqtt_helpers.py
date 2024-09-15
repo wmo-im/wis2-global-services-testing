3ced13608e96a20ac9491322a444bf2a30922f18
jmaxmarno <jmaxmarno@gmail.com>
2024-09-15 10:09:54 -0600

    allow insecure (no cert validation) for testing

--- tests/shared_utils/mqtt_helpers.py
+++ tests/shared_utils/mqtt_helpers.py
@@ -15,6 +15,7 @@ def check_broker_connectivity(connection_string: str):
     # return True if connection is successful, False otherwise
     is_connected = False
     pwclient = MQTTPubSubClient(connection_string)
+    pwclient.conn.tls_insecure_set(True)  # Disable certificate verification
     pwclient.conn.loop()
     if pwclient.conn.is_connected():
         is_connected = True
